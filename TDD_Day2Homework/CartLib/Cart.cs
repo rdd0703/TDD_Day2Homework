30efe2524836df08fc9d83e1136dda347eb54a50
HenryFang <henry@HENRYFANG1838>
2017-08-30 20:57:58 +0800

    案例一：紅燈

---
+++ TDD_Day2Homework/CartLib/Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartLib
+{
+    public class Cart
+    {
+        private List<CartItem> _carts;
+
+        public Cart(List<CartItem> carts)
+        {
+            this._carts = carts;
+        }
+
+        public double Calculate()
+        {
+            return 0;
+        }
+    }
+
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string BookName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ TDD_Day2Homework/CartLibTests/CartTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CartLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CartLib.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void CalculateTest_第一集買了一本_其他都沒買_價格應為_100元()
+        {
+            var carts = new List<CartItem>() {
+                new CartItem() { Id = 1 ,BookName = "哈利波特 第一集" ,Price = 100 ,Quantity = 1 }
+            };
+
+            var target = new Cart(carts);
+            var actual = target.Calculate();
+
+            var expected = 100;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
