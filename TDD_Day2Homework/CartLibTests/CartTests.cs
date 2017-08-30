a0015dc75d59c7e10343faa1b7e861ea421c0c41
HenryFang <henry@HENRYFANG1838>
2017-08-30 21:11:08 +0800

    案例三：紅燈

--- TDD_Day2Homework/CartLibTests/CartTests.cs
+++ TDD_Day2Homework/CartLibTests/CartTests.cs
@@ -41,5 +41,22 @@ namespace CartLib.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void CalculateTest_一二三集各買了一本_價格應為_270()
+        {
+            var carts = new List<CartItem>() {
+                new CartItem() { Id = 1 ,BookName = "哈利波特 第一集" ,Price = 100 ,Quantity = 1 },
+                new CartItem() { Id = 2 ,BookName = "哈利波特 第二集" ,Price = 100 ,Quantity = 1 },
+                new CartItem() { Id = 3 ,BookName = "哈利波特 第三集" ,Price = 100 ,Quantity = 1 }
+            };
+
+            var target = new Cart(carts);
+            var actual = target.Calculate();
+
+            var expected = 270;
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
